c3f5ae7804a91f62b555fae0d1808a0251621144
qq <qq@qq.com>
2017-06-02 12:28:59 +0800

    让分类支持独立模型

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -69,7 +69,7 @@ class ArticleController extends AdminController {
         //获取动态分类
         $cate_auth  =   AuthGroupModel::getAuthCategories(UID); //获取当前用户所有的内容权限节点
         $cate_auth  =   $cate_auth == null ? array() : $cate_auth;
-        $cate       =   M('Category')->where(array('status'=>1))->field('id,title,pid,allow_publish')->order('pid,sort')->select();
+        $cate       =   M('Category')->where(array('status'=>1))->field('id,title,pid,allow_publish,model')->order('pid,sort')->select();
 
         //没有权限的分类则不显示
         if(!IS_ROOT){
@@ -94,35 +94,47 @@ class ArticleController extends AdminController {
 
         //生成每个分类的url
         foreach ($cate as $key=>&$value){
-            $value['url']   =   'Article/index?cate_id='.$value['id'];
-            if($cate_id == $value['id'] && $hide_cate){
-                $value['current'] = true;
-            }else{
-                $value['current'] = false;
+            //获取model信息
+            $isIndependent = false;
+            if($value['model'] && is_numeric($value['model'])){
+                $rModel = M('Model')->find($value['model']);
+                if($rModel['extend'] == 0){
+                    $isIndependent = true;
+                }
             }
-            if(!empty($value['_child'])){
-                $is_child = false;
-                foreach ($value['_child'] as $ka=>&$va){
-                    $va['url']      =   'Article/index?cate_id='.$va['id'];
-                    if(!empty($va['_child'])){
-                        foreach ($va['_child'] as $k=>&$v){
-                            $v['url']   =   'Article/index?cate_id='.$v['id'];
-                            $v['pid']   =   $va['id'];
-                            $is_child = $v['id'] == $cate_id ? true : false;
+            if($isIndependent){
+                $value['url'] = 'think/lists?model='.$rModel['name'].'&category_id='.$value['id'];
+            }else{
+                $value['url']   =   'Article/index?cate_id='.$value['id'];
+                if($cate_id == $value['id'] && $hide_cate){
+                    $value['current'] = true;
+                }else{
+                    $value['current'] = false;
+                }
+                if(!empty($value['_child'])){
+                    $is_child = false;
+                    foreach ($value['_child'] as $ka=>&$va){
+                        $va['url']      =   'Article/index?cate_id='.$va['id'];
+                        if(!empty($va['_child'])){
+                            foreach ($va['_child'] as $k=>&$v){
+                                $v['url']   =   'Article/index?cate_id='.$v['id'];
+                                $v['pid']   =   $va['id'];
+                                $is_child = $v['id'] == $cate_id ? true : false;
+                            }
                         }
-                    }
-                    //展开子分类的父分类
-                    if($va['id'] == $cate_id || $is_child){
-                        $is_child = false;
-                        if($hide_cate){
-                            $value['current']   =   true;
-                            $va['current']      =   true;
+                        //展开子分类的父分类
+                        if($va['id'] == $cate_id || $is_child){
+                            $is_child = false;
+                            if($hide_cate){
+                                $value['current']   =   true;
+                                $va['current']      =   true;
+                            }else{
+                                $value['current']   =   false;
+                                $va['current']      =   false;
+                            }
                         }else{
-                            $value['current']   =   false;
                             $va['current']      =   false;
                         }
-                    }else{
-                        $va['current']      =   false;
                     }
                 }
             }
